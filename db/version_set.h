feefe3102810c79c8e2b335644c37f7ee7aa1050
ZhaoMing <zhaoming.274@bytedance.com>
2021-12-10 19:38:08 +0800

    Add TableEvictType & TableReader::ForceEvict

--- db/version_set.h
+++ db/version_set.h
@@ -732,8 +732,6 @@ class Version : public SeparateHelper, private LazyBufferState {
 
   ColumnFamilyData* cfd() const { return cfd_; }
 
-  void ForEachVersionList(void (*callback)(Version*), void* args);
-
   // Return the next Version in the linked list.
   Version* Next() const { return next_; }
 
@@ -826,10 +824,14 @@ class Version : public SeparateHelper, private LazyBufferState {
 struct ObsoleteFileInfo {
   FileMetaData* metadata;
   std::string path;
+  std::shared_ptr<TableCache> table_cache;
 
   ObsoleteFileInfo() noexcept : metadata(nullptr) {}
   ObsoleteFileInfo(FileMetaData* f, const std::string& file_path)
       : metadata(f), path(file_path) {}
+  ObsoleteFileInfo(FileMetaData* f, const std::string& file_path,
+                   std::shared_ptr<TableCache>& _table_cache)
+      : metadata(f), path(file_path), table_cache(_table_cache) {}
 
   ObsoleteFileInfo(const ObsoleteFileInfo&) = delete;
   ObsoleteFileInfo& operator=(const ObsoleteFileInfo&) = delete;
@@ -841,6 +843,7 @@ struct ObsoleteFileInfo {
   ObsoleteFileInfo& operator=(ObsoleteFileInfo&& rhs) noexcept {
     path = std::move(rhs.path);
     metadata = rhs.metadata;
+    table_cache = std::move(rhs.table_cache);
     rhs.metadata = nullptr;
 
     return *this;
@@ -859,7 +862,7 @@ class BaseReferencedVersionBuilder;
 class VersionSet {
  public:
   VersionSet(const std::string& dbname, const ImmutableDBOptions* db_options,
-             const EnvOptions& env_options, bool seq_per_batch,
+             const EnvOptions* env_options, bool seq_per_batch,
              Cache* table_cache, WriteBufferManager* write_buffer_manager,
              WriteController* write_controller);
   ~VersionSet();
@@ -1085,7 +1088,7 @@ class VersionSet {
   // This function doesn't support leveldb SST filenames
   void GetLiveFilesMetaData(std::vector<LiveFileMetaData>* metadata);
 
-  void GetObsoleteFiles(std::vector<ObsoleteFileInfo>* files,
+  void GetObsoleteFiles(chash_map<uint64_t, ObsoleteFileInfo>* files,
                         std::vector<std::string>* manifest_filenames,
                         uint64_t min_pending_output);
 
